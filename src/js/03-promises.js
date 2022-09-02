28cabb1d1e6a70d1a912ec4a63326d8f26a7dfe8
Elena-Sea <smetyukelena@gmail.com>
2022-09-02 14:05:52 +0200

    adds code for the task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,38 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import 'notiflix/dist/notiflix-3.2.5.min.css';
+
+const form = document.querySelector('form');
+form.addEventListener('submit', getPromiseResult);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (Math.random() > 0.3) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay)
+  });
+}
+
+function getPromiseResult(e) { 
+  e.preventDefault();
+  let promiseFirstDelay = Number(e.currentTarget.delay.value);
+  const promiseDelayStep = Number(e.currentTarget.step.value);
+  const promiseAmount = Number(e.currentTarget.amount.value);
+
+  for (let i = 0; i < promiseAmount; i += 1) { 
+    if (i > 0) {
+      promiseFirstDelay += promiseDelayStep;
+    }
+
+  createPromise(i + 1, promiseFirstDelay)
+  .then(({ position, delay }) => {
+      Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+  .catch(({ position, delay }) => {
+      Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+  }   
 }
