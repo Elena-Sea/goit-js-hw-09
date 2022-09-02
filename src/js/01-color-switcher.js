88cec13ab018475cb740f88600540c851a2f0651
Elena-Sea <smetyukelena@gmail.com>
2022-09-02 13:52:32 +0200

    adds code for task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+const TIMER_PERIOD = 1000;
+let timerId = null;
 
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick() { 
+    startBtn.disabled = true;
+    timerId = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+    }, TIMER_PERIOD);
+}
+
+function onStopBtnClick() { 
+    startBtn.disabled = false;
+    clearInterval(timerId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
